1b5ba02128650769d4c854786a31f7c4ea3dba9e
Dominik <d.jasinski@ideo.pl>
2024-04-04 15:51:40 +0200

    feat: update eslint config

--- frontend/.eslintrc.cjs
+++ frontend/.eslintrc.cjs
@@ -1,21 +1,53 @@
 module.exports = {
-  root: true,
-  env: { browser: true, es2020: true },
-  extends: [
-    'eslint:recommended',
-    'plugin:react/recommended',
-    'plugin:react/jsx-runtime',
-    'plugin:react-hooks/recommended',
-  ],
-  ignorePatterns: ['dist', '.eslintrc.cjs'],
-  parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
-  settings: { react: { version: '18.2' } },
-  plugins: ['react-refresh'],
-  rules: {
-    'react/jsx-no-target-blank': 'off',
-    'react-refresh/only-export-components': [
-      'warn',
-      { allowConstantExport: true },
-    ],
-  },
-}
+   root: true,
+   env: {
+      node: true,
+      browser: true,
+      es6: true,
+   },
+   extends: [
+      'eslint:recommended',
+      'plugin:react/recommended',
+      'plugin:react/jsx-runtime',
+      'plugin:react-hooks/recommended',
+   ],
+   ignorePatterns: ['dist', '.eslintrc.js'],
+   parserOptions: {
+      ecmaVersion: 'latest',
+      sourceType: 'module',
+      ecmaFeatures: {
+         jsx: true,
+      },
+   },
+   settings: {
+      react: {
+         version: 'detect',
+      },
+   },
+   plugins: ['react-refresh', 'prettier'],
+   rules: {
+      'import/prefer-default-export': 0,
+      'no-console': 'warn',
+      'no-nested-ternary': 0,
+      'no-underscore-dangle': 0,
+      'no-unused-expressions': ['error', { allowTernary: true }],
+      camelcase: 0,
+      'react/self-closing-comp': 1,
+      'react/jsx-filename-extension': [1, { extensions: ['.js', 'jsx'] }],
+      'react/prop-types': 0,
+      'react/destructuring-assignment': 0,
+      'react/jsx-no-comment-textnodes': 0,
+      'react/jsx-props-no-spreading': 0,
+      'react/no-array-index-key': 0,
+      'react/no-unescaped-entities': 0,
+      'react/require-default-props': 0,
+      'react/react-in-jsx-scope': 0,
+      'linebreak-style': ['error', 'unix'],
+      semi: ['error', 'always'],
+      'prettier/prettier': [
+         'error',
+         { endOfLine: 'auto' },
+         { usePrettierrc: true },
+      ],
+   },
+};
